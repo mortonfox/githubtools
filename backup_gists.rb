a171cc962cbe725d378bee711883924c9eb2aa76
Morton Fox <github@qslw.com>
2016-03-04 00:04:50 -0500

    Add script to back up gists.

---
+++ backup_gists.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+require 'octokit'
+require 'git'
+
+GISTS_FOLDER = './gists'
+
+def backup_gists username
+  # Check if netrc gem is installed.
+  got_netrc = Gem::Specification.find_all_by_name('netrc').any?
+
+  client = Octokit::Client.new(
+    auto_paginate: true,
+    netrc: got_netrc
+  )
+
+  gists = client.gists username
+  if gists.empty?
+    puts 'No gists to download.'
+    return
+  end
+
+  Dir.mkdir GISTS_FOLDER unless Dir.exist? GISTS_FOLDER
+  Dir.chdir GISTS_FOLDER
+
+  gists.each_with_index { |gist, git_indx|
+    puts "#{git_indx + 1}: Cloning gist #{gist[:id]} - #{(gist[:files].first || []).first}..."
+
+    # Find a subfolder name that does not already exist.
+    subfolder = gist[:id]
+    if Dir.exist? subfolder
+      i = 1
+      i += 1 while Dir.exist? "#{subfolder}-#{i}"
+      subfolder = "#{subfolder}-#{i}"
+    end
+
+    Git.clone gist[:git_pull_url], subfolder
+  }
+end
+
+if ARGV.size < 1
+  puts <<-EOM
+Usage: #{File.basename $PROGRAM_NAME} username
+  EOM
+  exit
+end
+
+backup_gists ARGV.first
+__END__
