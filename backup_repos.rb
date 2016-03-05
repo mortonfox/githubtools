916d90c831994d9c1b7cfca5254ad89b985a17f5
Morton Fox <github@qslw.com>
2016-03-05 00:44:41 -0500

    Add script to backup a user's repos.

---
+++ backup_repos.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'octokit'
+require 'git'
+
+REPOS_FOLDER = './repos'
+
+def backup_repos username
+  # Check if netrc gem is installed.
+  got_netrc = Gem::Specification.find_all_by_name('netrc').any?
+
+  client = Octokit::Client.new(
+    auto_paginate: true,
+    netrc: got_netrc
+  )
+
+  repos = client.repos username
+
+  # Backup only our own repos, not forks.
+  repos.reject! { |repo| repo[:fork] }
+
+  if repos.empty?
+    puts 'No repositories to download.'
+    return
+  end
+
+  Dir.mkdir REPOS_FOLDER unless Dir.exist? REPOS_FOLDER
+  Dir.chdir REPOS_FOLDER
+
+  repos.each_with_index { |repo, repo_indx|
+    puts "#{repo_indx + 1}: Cloning repo #{repo[:full_name]}..."
+
+    # Find a subfolder name that does not already exist.
+    subfolder = repo[:name]
+    if Dir.exist? subfolder
+      i = 1
+      i += 1 while Dir.exist? "#{subfolder}-#{i}"
+      subfolder = "#{subfolder}-#{i}"
+    end
+
+    got_warning = false
+
+    Git.clone repo[:clone_url], subfolder
+
+    result = system "cd \"#{subfolder}\"; git bundle create \"../#{subfolder}.bundle\" --all"
+    unless result
+      warn "git bundle failed for subfolder #{subfolder}: exit code #{$CHILD_STATUS}"
+      got_warning = true
+    end
+
+    result = system "zip -9qr \"#{subfolder}.zip\" \"#{subfolder}\""
+    unless result
+      warn "zip failed for subfolder #{subfolder}: exit code #{$CHILD_STATUS}"
+      got_warning = true
+    end
+
+    FileUtils.rm_rf subfolder unless got_warning
+  }
+end
+
+if ARGV.size < 1
+  puts <<-EOM
+Usage: #{File.basename $PROGRAM_NAME} username
+  EOM
+  exit
+end
+
+backup_repos ARGV.first
+__END__
