34d98a06cb6b295589427068fcd7048b3793ab21
Morton Fox <github@qslw.com>
2019-01-11 16:41:30 -0500

    Add search_and_del script

---
+++ search_and_del.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'octokit'
+
+RESULTS_SLICE = 50
+
+def search_repos client, search_str
+  repos = client.repos(nil)
+  repos.select { |repo| repo[:fork] && repo[:name].downcase.include?(search_str.downcase) }.map { |repo| repo[:full_name] }
+end
+
+def delete_repos client, reponames
+  reponames.each { |reponame|
+    puts "Deleting #{reponame}..."
+    client.delete_repo(reponame)
+  }
+end
+
+if ARGV.empty?
+  warn <<-WARNMSG
+Usage: #{File.basename $PROGRAM_NAME} search-string
+  WARNMSG
+  exit 1
+end
+
+# Check if netrc gem is installed.
+got_netrc = Gem::Specification.find_all_by_name('netrc').any?
+
+client = Octokit::Client.new(
+  auto_paginate: true,
+  netrc: got_netrc
+)
+
+results = search_repos(client, ARGV.first)
+
+results.each_slice(RESULTS_SLICE) { |rslice|
+  puts 'Found the following repos:'
+  rslice.each_with_index { |reponame, i| puts "#{i + 1}: #{reponame}" }
+  print "Enter 'yes' to delete: "
+
+  break unless $stdin.gets.strip.casecmp?('yes')
+
+  delete_repos(client, rslice)
+}
+
+__END__
